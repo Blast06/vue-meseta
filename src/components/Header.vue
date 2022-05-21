74ec2a93eb91f761c1ccaca3f433ff660b9830c5
blast06 <lacalculadora06@hotmail.com>
2022-05-21 16:55:15 -0400

    se creo header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+
+
+<template lang="spanish">
+    <div>
+        <header>
+            <nav>
+                <section class="contenedor nav">
+                
+                    <div class="logo">
+                        <img src="@/assets/imgs/logo.png" alt="">
+                    </div>
+                    <div class="enlaces-header">
+                        <a href="#">Inicio</a>
+                        <a href="#">Inicio</a>
+                        <a href="#">Inicio</a>
+                        <a href="#">Inicio</a>
+                        <a href="#">Inicio</a>
+                    </div>
+
+                </section>
+            </nav> 
+        </header>   
+    </div>
+</template>
+<script>
+export default {
+    name:'HeaderVue'
+    
+}
+</script>
+<style lang="spanish">
+    
+</style>
