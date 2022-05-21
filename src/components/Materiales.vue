1f8dedd40911fdc7b9383024c044831b1ad77af9
blast06 <lacalculadora06@hotmail.com>
2022-05-21 16:55:30 -0400

    se creo el componente de materiales

---
+++ src/components/Materiales.vue
@@ -0,0 +1,49 @@
+<template lang="spanish">
+    <div>
+        <p>Estos son los materiales</p>
+    </div>
+</template>
+<script>
+
+import { ref } from '@vue/reactivity';
+
+export default {
+    name:'MaterialesVue',
+
+
+
+
+
+    setup() {
+          const materiales = ref( [
+          {
+              id: 1,
+              url: require('@/assets/imgs/forma1.png'),
+              nombre_material: 'Granito negro ubatua',
+              precio: 250
+
+          },
+          {
+              id: 2,
+              url: require('@/assets/imgs/forma2.png'),
+              nombre_material: 'Granito negro ubatua',
+              precio: 350
+
+          },
+          {
+              id: 3,
+              url: require('@/assets/imgs/forma3.png'),
+              nombre_material: 'Granito negro ubatua',
+              precio: 250
+
+          }
+      ]);
+      return {materiales};
+    }
+
+    
+}
+</script>
+<style lang="spanish">
+    
+</style>
