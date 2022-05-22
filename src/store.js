c76a88c33e13680c067ea2a14d688749bf0aad0f
blast06 <lacalculadora06@hotmail.com>
2022-05-22 17:18:30 -0400

    se agrego metodo de crearmaterial

--- src/store.js
+++ src/store.js
@@ -6,11 +6,24 @@ export const store = reactive({
   tipo_meseta: 0,
   tamano_de_arreglo_mesetas: 0,
   escogioMateriales: false,
+  material: {
+    precio: 0,
+    nombre: "nada",
+  },
 
   changeTipoMeseta(tipo, tamano) {
     this.tamano_de_arreglo_mesetas = tamano;
     this.tipo_meseta = tipo;
-    console.log("tipo de meseta changetipo:", this.tipo_meseta);
-    console.log("Tamano de meseta changetipo:", this.tamano_de_arreglo_mesetas);
+    console.log("tipo de meseta tipo_meseta:", this.tipo_meseta);
+    console.log(
+      "Tamano de meseta tamano_de_arreglo_mesetas:",
+      this.tamano_de_arreglo_mesetas
+    );
+  },
+
+  crearTipoDeMaterial(precio, nombre) {
+    this.material.nombre = nombre;
+    this.material.precio = precio;
+    console.log(this.material);
   },
 });
