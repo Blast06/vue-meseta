a24b664ae4f7ec02022e38482030dd0ded7cbd06
blast06 <lacalculadora06@hotmail.com>
2022-05-22 18:00:15 -0400

    se agregaron acciones para mostrar pantallas

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,19 +1,31 @@
 <template lang="spanish">
     <div>
-        <div class="contenedor">
-                <section class="contenido-header">
+        <div class="contenedor ">
+                <section class="contenido-header justify-center">
                     <section class="texts-header">
+                        <img height="300" width="300" src="@/assets/imgs/meseta_disenando.svg" >
                         <h1>Disena tu meseta</h1>
                         <p>Elige el material, ingresa las medidas y haz el calculo</p>
-                        <a href="#">Empezar</a>
+                        <a @click="escogerMateriales()" >Empezar</a>
                     </section>
                 </section>
             </div>
     </div>
 </template>
 <script>
+// import { ref } from "@vue/reactivity";
+
+//almacena variables y metodos globales
+import { store } from "../store.js";
+
 export default {
   name: "SectionVue",
+
+  methods: {
+    escogerMateriales() {
+      store.escogerMateriales();
+    },
+  },
 };
 </script>
-<style lang="spanish"></style>
+<style></style>
