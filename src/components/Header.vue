d15092af2bb2f0f75caccfb459c13abd8fb5c64f
blast06 <lacalculadora06@hotmail.com>
2022-05-22 17:19:33 -0400

    se le dio estilo en el header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,33 +1,25 @@
-
-
-<template lang="spanish">
-    <div>
-        <header>
-            <nav>
-                <section class="contenedor nav">
-                
-                    <div class="logo">
-                        <img src="@/assets/imgs/logo.png" alt="">
-                    </div>
-                    <div class="enlaces-header">
-                        <a href="#">Inicio</a>
-                        <a href="#">Inicio</a>
-                        <a href="#">Inicio</a>
-                        <a href="#">Inicio</a>
-                        <a href="#">Inicio</a>
-                    </div>
-
-                </section>
-            </nav> 
-        </header>   
-    </div>
+<template>
+  <div class="mb-5">
+    <header>
+      <nav>
+        <section class="contenedor nav">
+          <div class="logo">
+            <img src="@/assets/imgs/logo.png" alt="" />
+          </div>
+          <div class="enlaces-header">
+            <a href="#">Inicio</a>
+            <a href="#">Contacto</a>
+            <a href="#">Sobre Nosotros</a>
+          </div>
+        </section>
+      </nav>
+      <!-- AQUI VA EL CONTENEDOR DEL SECTION -->
+    </header>
+  </div>
 </template>
 <script>
 export default {
-    name:'HeaderVue'
-    
-}
+  name: "HeaderVue",
+};
 </script>
-<style lang="spanish">
-    
-</style>
+<style lang="spanish"></style>
