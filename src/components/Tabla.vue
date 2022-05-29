9b7245b27dc124f2728771a01b3d02f859bfc559
blast06 <lacalculadora06@hotmail.com>
2022-05-29 12:01:25 -0400

    Se agrego JSDPF para generar PDF

--- src/components/Tabla.vue
+++ src/components/Tabla.vue
@@ -75,30 +75,34 @@
             </td>
           </tr>
           <tr class="bg-lime-500">
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
+            <td class="p-3 text-sm font-bold  text-gray-700 whitespace-nowrap">
               TOTAL
             </td>
             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-              Kring New Fit office chair, mesh + PU, black
+             
             </td>
             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
               <span
-                class="p-1.5 text-xs font-medium uppercase tracking-wider text-gray-800 bg-gray-200 rounded-lg bg-opacity-50"
-                >Cancelled</span
+                class="p-1.5 text-xs uppercase tracking-wider text-gray-800 rounded-lg bg-opacity-50"
+                ></span
               >
             </td>
             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-              16/10/2021
+             
             </td>
-            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">$200.00</td>
+            <td class="p-3 text-sm font-weight-bold text-gray-700 whitespace-nowrap">
+            {{costCurrency}}    
+            </td>
           </tr>
         </tbody>
       </table>
+      
     </div>
+    <!-- <p class=" mr-5 text-right">Itbis inlcuido</p> -->
 
     <!-- Boton de generar PDF -->
     <button
-      class="text-green-500 hover:text-white py-2 px-4 uppercase rounded bg-white border border-green-500 hover:bg-green-600 shadow-none hover:shadow-lg font-medium transition duration-200"
+      @click="generatePDF()" class="text-green-500 hover:text-white py-2 px-4 uppercase rounded bg-white border border-green-500 hover:bg-green-600 shadow-none hover:shadow-lg font-medium transition duration-200"
     >
       Generar PDF
     </button>
@@ -106,21 +110,108 @@
 </template>
 <script>
 import { useRoute } from "vue-router";
+const formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'})
+import { jsPDF } from "jspdf";
+import autoTable from 'jspdf-autotable';
+
+
+
+
+
 
 export default {
   name: "TablaVue",
+  components: {
+    
+    },
   data() {
     return {
       cotizacion: {},
       total: 0,
+      heading: "Cotizacion Meseta",
+      items: [
+        
+      ]
+     
     };
   },
+
 
   mounted() {
     const route = useRoute();
     this.cotizacion = route.params;
     console.warn("Desde la ruta", this.cotizacion);
+    this.items = [
+      "10001",this.cotizacion.NombreMaterial, 
+       this.cotizacion.totalMedidas, 
+       this.cotizacion.precioMaterial, 
+       this.cotizacion.CostoTotal
+    ]
   },
+
+  computed: {
+        costCurrency() {
+            return formatter.format(this.cotizacion.total)
+        }
+    },
+
+    methods: {
+      generatePDF(){
+       
+      const doc = new jsPDF({
+        orientation: "portrait",
+        unit: "in",
+        format: "letter",
+      
+      });
+      // text is placed using x, y coordinates
+      // doc.setFontSize(16).text(this.heading, 0.5, 1.0);
+      // create a line under heading
+      // doc.setLineWidth(0.01).line(0.5, 1.1, 8.0, 1.1);
+      // Using autoTable plugin
+
+
+      
+ 
+     
+     
+        
+
+
+        //TODO: APLICAR FUNCION DE CONVERTIR EN MONEDA A LOS CAMPOS DE MONEDA.
+        //TODO: AGREGAR UNA TERCERA FILA QUE MUESTRE EL TOTAL
+        //CAMBIAR EL COLOR AL VERDE QUE TIENE LA TABLA EN VUE
+
+        autoTable(doc, {
+          head: [['No', 'Detalles', 'Medidas', 'Precio', 'Total']],
+          body: [
+                  ["1001", this.cotizacion.NombreMaterial, `${this.cotizacion.totalMedidas}mts`, `RD$ ${this.cotizacion.precioMaterial}`, ` RD$${this.cotizacion.CostoTotal}`  ],
+                  ["1002", 'Transporte - Combustible', '-',`RD$${this.cotizacion.Combustible}`, `RD$${this.cotizacion.Combustible}`  ],
+                  
+                  // ...
+                ],
+        })
+      
+
+
+
+      // Creating footer and saving file
+      // doc
+      //   .setFont("times")
+      //   .setFontSize(11)
+      //   .setFontStyle("italic")
+      //   .setTextColor(0, 0, 255)
+      //   .text(
+      //     "This is a simple footer located .5 inches from page bottom",
+      //     0.5,
+      //     doc.internal.pageSize.height - 0.5
+      //   )
+      //   .save(`${this.heading}.pdf`);
+      doc.setFontSize(16).text(this.heading, 0.5, 0.5);
+      doc.setFont("times").setFontSize(11).setTextColor(0,0,255).save(`${this.heading}.pdf`);
+    
+      }
+    },
 };
 </script>
 <style lang="spanish"></style>
